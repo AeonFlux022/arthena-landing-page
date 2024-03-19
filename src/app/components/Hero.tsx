fd56058a1ce5e41bf033234ad4f269b02fc36f45
AeonFlux022 <tolentinokevinjan22@gmail.com>
2024-03-19 22:58:19 +0800

    replace hero to dvh

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,11 +1,11 @@
 const Hero = () => {
   return (
     <>
-    <header className="h-96 bg-center bg-cover bg-primary text-white">
-      <section className="flex h-full">
-        <div className="flex flex-col justify-center w-6/12">
-          <h1 className="text-6xl leading-[4rem]">Empowering our student artists</h1>
-          <p className="text-2xl text-balance">Join our community of emerging artist and share your art to ignite inspiration.</p>
+    <header className="h-dvh bg-center bg-cover bg-primary text-white">
+      <section className="flex h-full px-0">
+        <div className="flex flex-col justify-center pl-14 w-6/12">
+          <h1 className="text-6xl leading-[4rem]">Empowering our student artists.</h1>
+          <p className="text-2xl text-balance mt-2">Join our community of emerging artist and share your art to ignite inspiration.</p>
           <button className="bg-secondary text-black font-bold w-44 p-4 mt-8">Explore Artworks</button>
         </div>
         <div className="flex w-6/12 bg-slate-600">
